322e888d896e85b19d832de9339f342bb338184c
tahahasan678@gmail.com <tahahasan678@gmail.com>
2026-08-17 22:18:06 -0400

    Add ALU input A mux; update gitignore

---
+++ rtl/alu_input_a_mux.sv
@@ -0,0 +1,20 @@
+module alu_input_a_mux (
+    input logic [31:0] rs1,
+    input logic [31:0] pc,
+    input logic [1:0] alu_a_sel,
+    output logic [31:0] out
+);
+
+   localparam [31:0] zero = 32'd0;
+
+    always_comb begin
+        case (alu_a_sel) 
+            2'b00: out = rs1;
+            2'b01: out = pc;
+            2'b10: out = zero;
+            
+            default: out = rs1;
+        endcase 
+    end
+
+endmodule
